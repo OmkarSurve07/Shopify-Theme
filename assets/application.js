4b05cda36ca8081f6a43dec364370c2b0f078be0
OmkarSurve07 <omkarsuhassurve@gmail.com>
2021-06-23 17:48:30 +0530

    forgot password setup

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,41 @@
 // Put your application javascript here
+
+if( document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change',function(e) {
+        var url = new URL(window.location.href);
+        url.searchParams.set('sort_by',e.currentTarget.value);
+
+        window.location = url.href;
+    });
+}
+
+if( document.getElementById('AddressCountryNew') != null) {
+    document.getElementById('AddressCountryNew').addEventListener('change', function(e) {
+        var provinces = this.options[this.selectedIndex].getAttribute('data-provinces');
+        var provinceSelector = document.getElementById('AddressCountryNew');
+        var provinceArray = JSON.parse(provinces);
+
+        // console.log(provinceArray);
+        if(provinceArray.length < 1) {
+            provinceSelector.setAttribute('disabled','disabled');
+        } else {
+            provinceSelector.removeAttribute('disabled');
+        }
+        provinceSelector.innerHTML = '';
+        var options = '';
+        for(var i = 0; i < provinceArray.length; i++){
+            options += <option value="' + provinceArray[i][0] + '">'+ provinceArray[i][0] +'</option>
+        }
+        provinceSelector.innerHTML = options;
+    });
+}
+
+if(document.getElementById("forgotPassword") != null ){
+    document.getElementById("forgotPassword").addEventListener("click", function(e) {
+        const element = document.querySelector("#forgot_password_form")
+        if(element.classList.contains("d-none")){
+            element.classList.remove("d-none")
+            element.classList.add("d-block")
+        }
+    })
+}
